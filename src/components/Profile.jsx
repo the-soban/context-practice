f69e956ddff3eb1ba9b52fc3afc69e0d24ecef8e
the-soban <sobanshafeq123@gmail.com>
2023-08-26 01:28:48 +0500

    added back to login button

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,13 +2,20 @@ import React, { useState, useContext } from 'react'
 import { LoginContext } from '../Contexts/LoginContext'
 
 const Profile = () => {
-    const { username, password } = useContext(LoginContext)
+    const { username, password, setShowProfile } = useContext(LoginContext)
 
     return (
         <div>
             <h1>Your Profile</h1>
             <h2>Username: {username}</h2>
             <h2>Password: {password}</h2>
+            <button
+                onClick={() => {
+                    setShowProfile(false)
+                }}
+            >
+                Back to Login
+            </button>
         </div>
     )
 }
